ffd53a5c87187d0fb7d8d6056d1dd23d4368f54c
Thomas Klytta <thomas.klytta@gmail.com>
2019-12-06 14:20:30 +0100

    Implemented message protocol

--- Chat/MainWindow.xaml.cs
+++ Chat/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Windows;
 
 namespace Chat
@@ -17,15 +18,56 @@ namespace Chat
         }
 
         TcpClient client;
+        string Username = string.Empty;
+
+        void ReceiveData(TcpClient client)
+        {
+            while (true)
+            {
+                var stream = client.GetStream();
+
+                var buffer = new byte[1024];
+                var byteCount = stream.Read(buffer, 0, buffer.Length);
+
+                if (byteCount > 0)
+                {
+                    string data = Encoding.UTF8.GetString(buffer, 0, byteCount);
+                    data = data.Replace("\0", string.Empty);
+
+                    var messageParts = data.Split('|');
+
+                    switch (messageParts[0])
+                    {
+                        case "message":
+                            Dispatcher.Invoke(() =>
+                            {
+                                ChatText.Text = messageParts[1] + ": " + messageParts[2] + Environment.NewLine + ChatText.Text;
+                            });
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
 
         private void ConnectButton_Click(object sender, RoutedEventArgs e)
         {
             try
             {
+                Username = NameInput.Text;
+
                 var ipAddress = IPAddress.Parse(IpAddressInput.Text);
                 client = new TcpClient();
                 client.Connect(ipAddress, 5000);
                 SendButton.IsEnabled = true;
+
+                SendData(string.Format("connect|{0}", Username));
+
+                ChatText.Text = string.Empty;
+
+                var thread = new Thread(() => ReceiveData(client));
+                thread.Start();
             }
             catch (FormatException)
             {
@@ -39,12 +81,15 @@ namespace Chat
 
         private void SendButton_Click(object sender, RoutedEventArgs e)
         {
-            var messageText = MessageInput.Text;
+            var messageText = string.Format("message|{0}|{1}", Username, MessageInput.Text);
             MessageInput.Text = string.Empty;
+            SendData(messageText);
+        }
 
+        private void SendData(string messageText)
+        {
             var stream = client.GetStream();
-
-            var messageTextBytes = Encoding.ASCII.GetBytes(messageText);
+            var messageTextBytes = Encoding.UTF8.GetBytes(messageText);
             stream.Write(messageTextBytes, 0, messageTextBytes.Length);
         }
     }
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -4,18 +4,36 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ChatServer
 {
     class Program
     {
         static List<TcpClient> Clients = new List<TcpClient>();
+        static bool ParallelExecution = false;
+        static Timer SendClientUpdateTimer;
 
         static void Main(string[] args)
         {
-            //var tcpListener = new TcpListener(5000); <- obsolete
+            Console.WriteLine("Soll der Server parallel verarbeiten? (y/n)");
+            var input = Console.ReadKey();
+            Console.WriteLine();
+
+            if (input.Key == ConsoleKey.Y)
+            {
+                Console.WriteLine("Parallele Verarbeitung aktiviert.");
+                ParallelExecution = true;
+            }
+            else
+            {
+                Console.WriteLine("Parallele Verarbeitung deaktiviert.");
+            }
+
             var tcpListener = new TcpListener(IPAddress.Any, 5000);
             tcpListener.Start();
+
+            SendClientUpdateTimer = new Timer(SendClientUpdate, null, 0, 1000);
 
             while (true)
             {
@@ -25,8 +43,16 @@ namespace ChatServer
                 var thread = new Thread(() => HandleClient(client));
                 thread.Start();
 
+                Console.WriteLine("Verbunden: {0}", client.Client.RemoteEndPoint);
                 Console.WriteLine("Es sind {0} Teilnehmer online.", Clients.Count);
             }
+        }
+
+        private static void SendClientUpdate(object state)
+        {
+            string message = string.Format("Es sind {0} Teilnehmer online", Clients.Count);
+            var byteMessage = Encoding.UTF8.GetBytes(message);
+            Broadcast(byteMessage);
         }
 
         static void HandleClient(TcpClient client)
@@ -40,8 +66,30 @@ namespace ChatServer
 
                 if (byteCount > 0)
                 {
-                    string data = Encoding.ASCII.GetString(buffer, 0, byteCount);
+                    Broadcast(buffer);
+
+                    string data = Encoding.UTF8.GetString(buffer, 0, byteCount);
                     Console.WriteLine(data);
+                }
+            }
+        }
+
+        private static void Broadcast(byte[] buffer)
+        {
+            if (ParallelExecution)
+            {
+                Parallel.ForEach(Clients, otherClient =>
+                {
+                    var otherStream = otherClient.GetStream();
+                    otherStream.Write(buffer, 0, buffer.Length);
+                });
+            }
+            else
+            {
+                foreach (var otherClient in Clients)
+                {
+                    var otherStream = otherClient.GetStream();
+                    otherStream.Write(buffer, 0, buffer.Length);
                 }
             }
         }
